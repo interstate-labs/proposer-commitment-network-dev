56dc9fc7773f6fcdfbefe61ed5e3740d1367b193
Irfan Shaik <YOUR_EMAIL>
2025-03-23 04:07:32 -0400

    create scripts

--- scripts/revoke-sidecar/src/main.rs
+++ scripts/revoke-sidecar/src/main.rs
@@ -49,6 +49,8 @@ async fn main() ->eyre::Result<()> {
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
+    let signer_type = env::var("SIGNER_TYPE").expect("please set a signer_type");
+
     let keys_path = env::var("KEYS_PATH").expect("couldn't find keys path in env file");
     let password_path = env::var("SECRETS_PATH").expect("couldn't find secrets path in env file");
     let out = env::var("OUT_FILE").expect("couldn't find out file in env file");
@@ -59,43 +61,66 @@ async fn main() ->eyre::Result<()> {
     let delegatee_pubkey:BlsPublicKey = parse_bls_public_key(delegate_pbukey_str.as_str()).expect("Invalid public key");
     let keystore_secret = KeystoreSecret::from_directory(password_path.as_str()).unwrap();
 
-    let signed_messages = generate_from_keystore(
-        &keys_path,
-        keystore_secret,
-        delegatee_pubkey.clone(),
-        Chain::Kurtosis,
-        Action::Delegate,
-    ).expect("Invalid signed message request");
 
-    let signed_messages_web3 = generate_from_web3signer(Web3SignerOpts{ url:web3signer_url}, delegatee_pubkey, Action::Delegate).await?;
+    if signer_type == "KEYSTORES" {
+        let signed_messages = generate_from_keystore(
+            &keys_path,
+            keystore_secret,
+            delegatee_pubkey.clone(),
+            Chain::Kurtosis,
+            Action::Revoke,
+        ).expect("Invalid signed message request");
 
-    debug!("Signed {} messages with keystore", signed_messages.len());
-    debug!("Signed {} messages with web3signature", signed_messages_web3.len());
+        // Verify signatures
+        for message in &signed_messages {
+            verify_message_signature(message, Chain::Kurtosis).expect("invalid signature");
+        }
 
-        
-    // Verify signatures
-    for message in &signed_messages {
-        verify_message_signature(message, Chain::Kurtosis).expect("invalid signature");
-    }
-        
-    write_to_file(out.as_str(), &signed_messages).expect("invalid file");
+        debug!("Signed {} messages with keystore", signed_messages.len());
 
-    write_to_file(out_web3.as_str(), &signed_messages_web3).expect("invalid file");
+        write_to_file(out.as_str(), &signed_messages).expect("invalid file");
 
-    let client = reqwest::ClientBuilder::new().build().unwrap();
+        let client = reqwest::ClientBuilder::new().build().unwrap();
 
-    let response = client
-        .post(relay_url + PERMISSION_DELEGATE_PATH)
-        .header("content-type", "application/json")
-        .body(serde_json::to_string(&signed_messages_web3)?)
-        .send()
-        .await?;
+        let response = client
+            .post(relay_url + PERMISSION_DELEGATE_PATH)
+            .header("content-type", "application/json")
+            .body(serde_json::to_string(&signed_messages)?)
+            .send()
+            .await?;
 
-        
-    if response.status() != StatusCode::OK {
-        error!("failed to send  delegations to relay");
-    } else {
-        info!("submited  {} delegations to relay", signed_messages_web3.len());
+        if response.status() != StatusCode::OK {
+            error!("failed to send  delegations to relay");
+        } else {
+            info!("submited  {} delegations to relay", signed_messages.len());
+        }
+
+    } else if signer_type == "WEB3SIGNER" {
+
+        let signed_messages_web3 = generate_from_web3signer(Web3SignerOpts{ url:web3signer_url}, delegatee_pubkey, Action::Revoke).await?;
+
+        // Verify signatures
+        for message in &signed_messages_web3 {
+            verify_message_signature(message, Chain::Kurtosis).expect("invalid signature");
+        }
+
+        debug!("Signed {} messages with web3signature", signed_messages_web3.len());
+        write_to_file(out_web3.as_str(), &signed_messages_web3).expect("invalid file");
+
+        let client = reqwest::ClientBuilder::new().build().unwrap();
+
+        let response = client
+            .post(relay_url + PERMISSION_DELEGATE_PATH)
+            .header("content-type", "application/json")
+            .body(serde_json::to_string(&signed_messages_web3)?)
+            .send()
+            .await?;
+
+        if response.status() != StatusCode::OK {
+            error!("failed to send  delegations to relay");
+        } else {
+            info!("submited  {} delegations to relay", signed_messages_web3.len());
+        }
     }
 
     Ok(())
